44465d66acf3dfb9c8bb78560d5a1ded22fd1d91
YuanShuoHsu <79447837+YuanShuoHsu@users.noreply.github.com>
2022-10-08 23:58:20 +0800

    navbar dataset jsx complete

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,38 +1,38 @@
 import React from 'react'
+
+import NAVS from "../../dataset/navbar"
+
 import { NavLink } from "react-router-dom"
+
 import "./index.scss"
 
 export default function Navbar() {
   return (
     <div className='navbar'>
       <ul className='menu'>
-        <li className='item'>
-          <NavLink className="link" to="1">1</NavLink>
-        </li>
-        <li className='item'>
-          <NavLink className="link">2
-            <svg className='caret-down' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
-              <path d="M137.4 374.6c12.5 12.5 32.8 12.5 45.3 0l128-128c9.2-9.2 11.9-22.9 6.9-34.9s-16.6-19.8-29.6-19.8L32 192c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9l128 128z" />
-            </svg>
-          </NavLink>
-          <ul className='subMenu'>
-            <li className='subItem'>
-              <NavLink className="subLink" to="2-1">2-1</NavLink>
+        {
+          NAVS && NAVS.map(item => (
+            <li className='item' key={item.id}>
+              <NavLink className="link" to={item.layers ? null : item.nav}>{item.nav}
+                {
+                  item.layers ?
+                    <svg className='caret-down' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+                      <path d="M137.4 374.6c12.5 12.5 32.8 12.5 45.3 0l128-128c9.2-9.2 11.9-22.9 6.9-34.9s-16.6-19.8-29.6-19.8L32 192c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9l128 128z" />
+                    </svg> : null
+                }
+              </NavLink>
+              <ul className='subMenu'>
+                {
+                  item.layers && item.layers.map(subItem => (
+                    <li className='subItem' key={subItem.subId}>
+                      <NavLink className="subLink" to={subItem.subnav}>{subItem.subnav}</NavLink>
+                    </li>
+                  ))
+                }
+              </ul>
             </li>
-            <li className='subItem'>
-              <NavLink className="subLink" to="2-2">2-2</NavLink>
-            </li>
-            <li className='subItem'>
-              <NavLink className="subLink" to="2-3">2-3</NavLink>
-            </li>
-          </ul>
-        </li>
-        <li className='item'>
-          <NavLink className="link" to="3">3</NavLink>
-        </li>
-        <li className='item'>
-          <NavLink className="link" to="4">4</NavLink>
-        </li>
+          ))
+        }
       </ul>
     </div>
   )
---
+++ src/dataset/navbar.js
@@ -0,0 +1,30 @@
+const NAVS = [
+    {
+        id: 1,
+        nav: "1",
+    },
+    {
+        id: 1,
+        nav: "2",
+        layers: [
+            {
+                subId: 1,
+                subnav: "2-1"
+            },
+            {
+                subId: 2,
+                subnav: "2-2"
+            },
+            {
+                subId: 3,
+                subnav: "2-3"
+            }
+        ]
+    },
+    {
+        id: 1,
+        nav: "3",
+    },
+]
+
+export default NAVS
