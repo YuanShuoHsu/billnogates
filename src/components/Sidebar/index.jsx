758eb46a82581460604e7afb466c741f93b980c6
YuanShuoHsu <79447837+YuanShuoHsu@users.noreply.github.com>
2022-10-20 19:06:25 +0800

    sidebar betterscroll

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,30 +1,38 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 
-import Navbar from "../Navbar"
+import SidebarScroll from '../SidebarScroll';
 
-import { useDispatch, useSelector } from 'react-redux';
-import { hide_sidebar } from '../../store/slice/sidebar';
+import { useSelector } from 'react-redux';
+
+import BScroll from '@better-scroll/core'
+import MouseWheel from '@better-scroll/mouse-wheel'
 
 import "./index.scss"
 
 export default function Sidebar() {
-  const dispatch = useDispatch();
+
   const sidebar = useSelector(state => state.sidebar.value);
 
-  const hideMenu = () => {
-    dispatch(hide_sidebar())
+  const scrollWrapper = useRef()
+
+  useEffect(() => {
+    newBetterScroll()
+  })
+
+  const newBetterScroll = () => {
+    BScroll.use(MouseWheel)
+    let bs = new BScroll(scrollWrapper.current, {
+      probeType: 3,
+      mouseWheel: true,
+    })
+    setInterval(() => {
+      bs.refresh()
+    }, 2000)
   }
 
   return (
-    <div className={`sidebar ${sidebar}`}>
-      <div className='landscape'>
-        <div onClick={hideMenu} className='close'>
-          <svg className='xmark' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
-            <path d="M310.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L160 210.7 54.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L114.7 256 9.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 301.3 265.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L205.3 256 310.6 150.6z" />
-          </svg>
-        </div>
-      </div>
-      <Navbar />
+    <div ref={scrollWrapper} className={`sidebar ${sidebar}`}>
+      <SidebarScroll />
     </div>
   )
 }
