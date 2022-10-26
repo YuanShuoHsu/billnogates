d7a98825cd98256781e555bfae27ecb1f91ca29f
YuanShuoHsu <79447837+YuanShuoHsu@users.noreply.github.com>
2022-10-26 17:08:37 +0800

    membership register scss modify box-sizing border-box

--- src/pages/Membership/Register/index.jsx
+++ src/pages/Membership/Register/index.jsx
@@ -5,17 +5,23 @@ import "./index.scss"
 export default function Register() {
     return (
         <div className='Register'>
-            <div className='name'>
-                <input className='lastName' type="text" placeholder='姓氏' />
-                <input className='firstName' type="text" placeholder='名稱' />
-            </div>
             <div className='privacy'>
-                <select className='gender' name="gender" required>
-                    <option value="" disabled selected>性別</option>
-                    <option value="male">男性</option>
-                    <option value="female">女性</option>
-                </select>
-                <input className='birthday' type="date" placeholder='出生日期' required />
+                <div className='grid'>
+                    <input className='lastName' type="text" placeholder='姓氏' />
+                </div>
+                <div className='grid'>
+                    <input className='firstName' type="text" placeholder='名稱' />
+                </div>
+                <div className='grid'>
+                    <select defaultValue={''} className='gender' name="gender" required>
+                        <option value="" disabled>性別</option>
+                        <option value="male">男性</option>
+                        <option value="female">女性</option>
+                    </select>
+                </div>
+                <div className='grid'>
+                    <input className='birthday' type="date" placeholder='出生日期' required />
+                </div>
             </div>
             <div className='information'>
                 <input className='email' type="text" placeholder='電子郵件' />
