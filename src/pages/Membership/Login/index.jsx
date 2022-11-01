4c84a445fb51da5fa4eb2b18bfc8209a96d3c450
YuanShuoHsu <79447837+YuanShuoHsu@users.noreply.github.com>
2022-11-01 16:47:54 +0800

    firebase login function add

--- src/pages/Membership/Login/index.jsx
+++ src/pages/Membership/Login/index.jsx
@@ -1,17 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import { useNavigate } from "react-router-dom"
+
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../../utils/firebase"
 
 import "./index.scss"
 
 export default function Login() {
+
+    const navigate = useNavigate()
+
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    const saveEmail = (event) => {
+        setEmail(event.target.value)
+    }
+
+    const savePassword = (event) => {
+        setPassword(event.target.value)
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        signInWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                // Signed in
+                console.log(userCredential)
+                const user = userCredential.user;
+                console.log(user)
+                alert("登入成功")
+                navigate("/")
+            })
+            .catch((error) => {
+                console.log(error)
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                console.log(errorCode)
+                console.log(errorMessage)
+            });
+    }
     return (
-        <div className='Login'>
+        <form className='Login' onSubmit={handleSubmit}>
             <div className='information'>
-                <input className='email' type="text" placeholder='電子郵件' />
-                <input className='password' type="password" placeholder='密碼' />
+                <input onChange={saveEmail} className='email' type="text" placeholder='電子郵件' />
+                <input onChange={savePassword} className='password' type="password" placeholder='密碼' />
             </div>
             <div className='loginContainer'>
                 <button className='loginButton'>登入</button>
             </div>
-        </div>
+        </form>
     )
 }
--- src/pages/Membership/Register/index.jsx
+++ src/pages/Membership/Register/index.jsx
@@ -1,16 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import { useNavigate } from "react-router-dom"
+
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../../utils/firebase"
 
 import "./index.scss"
 
 export default function Register() {
+
+    const navigate = useNavigate()
+
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    const saveEmail = (event) => {
+        setEmail(event.target.value)
+    }
+
+    const savePassword = (event) => {
+        setPassword(event.target.value)
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        createUserWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                // Signed in
+                console.log(userCredential)
+                const user = userCredential.user;
+                console.log(user)
+                alert("註冊成功")
+                navigate("/membership/login")
+            })
+            .catch((error) => {
+                console.log(error)
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                console.log(errorCode)
+                console.log(errorMessage)
+            });
+    }
+
     return (
-        <div className='Register'>
+        <form className='Register' onSubmit={handleSubmit}>
             <div className='privacy'>
                 <div className='grid'>
-                    <input className='lastName' type="text" placeholder='姓氏' />
+                    <input className='lastName' type="text" placeholder='姓氏' required />
                 </div>
                 <div className='grid'>
-                    <input className='firstName' type="text" placeholder='名稱' />
+                    <input className='firstName' type="text" placeholder='名稱' required />
                 </div>
                 <div className='grid'>
                     <select defaultValue={''} className='gender' name="gender" required>
@@ -24,12 +63,12 @@ export default function Register() {
                 </div>
             </div>
             <div className='information'>
-                <input className='email' type="text" placeholder='電子郵件' />
-                <input className='password' type="password" placeholder='密碼' />
+                <input onChange={saveEmail} className='email' type="text" placeholder='電子郵件' required />
+                <input onChange={savePassword} className='password' type="password" placeholder='密碼' required />
             </div>
             <div className='registerContainer'>
                 <button className='registerButton'>註冊</button>
             </div>
-        </div>
+        </form>
     )
 }
