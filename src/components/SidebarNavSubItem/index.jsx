87a950823e7630706690ec858879e5290b48ba6d
YuanShuoHsu <79447837+YuanShuoHsu@users.noreply.github.com>
2022-12-16 00:34:47 +0800

    sidebar components list scss modify align

--- src/components/SidebarNavSubItem/index.jsx
+++ src/components/SidebarNavSubItem/index.jsx
@@ -36,7 +36,7 @@ export default function SidebarNavSubItem(props) {
                         </NavLink>
                     </li> :
                     <li onClick={(event) => handleClick(event, !subItemClick)} className={`${styles.SidebarNavSubItem} ${subItemClick ? `${styles.active}` : ""}`} key={subItem.subId}>
-                        <div className={styles.subLink}>
+                        <div className={`${styles.subLink} ${styles.active}`}>
                             <span className={styles.subText}>{subItem.subNav}</span>
                             <svg className={styles.svg} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
                                 <path d="M201.4 374.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 306.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z" />
