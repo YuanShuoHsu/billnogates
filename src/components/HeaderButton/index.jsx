4407d2ce6762c04225caea7e429a728dbddcc426
YuanShuoHsu <ook1725@gmail.com>
2024-07-26 17:58:18 +0800

    remittance modified

--- src/components/HeaderButton/index.jsx
+++ src/components/HeaderButton/index.jsx
@@ -1,16 +1,15 @@
 import { useState, useEffect } from "react";
-
-import { useLocation } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useLocation } from "react-router-dom";
 
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "../../utils/firebase";
 
-import { Link } from "react-router-dom";
-
-import { useDispatch, useSelector } from "react-redux";
-import { showSearch, hideSearch } from "../../store/slice/search";
 import { showCartbar } from "../../store/slice/cartbar";
+import { showSearch, hideSearch } from "../../store/slice/search";
 import { showSidebar } from "../../store/slice/sidebar";
+
+import { ReactComponent as Bars } from "../../images/others/bars.svg";
 
 import styles from "./index.module.scss";
 
@@ -108,7 +107,7 @@ export default function HeaderButton() {
   const renderCartbarNumber = () => {
     return cartbarItem.reduce((count, item) => count + item.number, 0);
   };
-  
+
   return (
     <div className={styles.headerButton}>
       <button
@@ -152,13 +151,7 @@ export default function HeaderButton() {
         className={`${styles.button} ${styles.hamburger}`}
       >
         <div className={styles.svgBox}>
-          <svg
-            className={styles.svg}
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 448 512"
-          >
-            <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" />
-          </svg>
+          <Bars className={styles.svg} />
         </div>
       </button>
     </div>
--- src/pages/Remittance/index.tsx
+++ src/pages/Remittance/index.tsx
@@ -1,18 +1,19 @@
 import { useState, useEffect } from "react";
-
 import { useNavigate, useLocation } from "react-router-dom";
 
 import { onAuthStateChanged } from "firebase/auth";
-import { auth, db } from "../../utils/firebase";
 import { doc, getDoc } from "firebase/firestore";
-
-import { formatDate, formatTime } from "../../utils/formatDateTime";
+import { auth, db } from "../../utils/firebase";
 
 import Cartbar from "../../components/Cartbar";
 import Sidebar from "../../components/Sidebar";
 import ScrollToTopButton from "../../components/ScrollToTopButton";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
+
+import { ReactComponent as CircleExclamation } from "../../images/others/circle-exclamation.svg";
+
+import { formatDate, formatTime } from "../../utils/formatDateTime";
 
 import styles from "./index.module.scss";
 
@@ -157,13 +158,7 @@ export default function Remittance() {
           <h2 className={styles.remittance__title}>您的訂單即將完成</h2>
           <div className={styles.remittance__quotation}>
             <div className={styles.remittance__svgBox}>
-              <svg
-                className={styles.remittance__svg}
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 512 512"
-              >
-                <path d="M256 512c141.4 0 256-114.6 256-256S397.4 0 256 0S0 114.6 0 256S114.6 512 256 512zm0-384c13.3 0 24 10.7 24 24V264c0 13.3-10.7 24-24 24s-24-10.7-24-24V152c0-13.3 10.7-24 24-24zm32 224c0 17.7-14.3 32-32 32s-32-14.3-32-32s14.3-32 32-32s32 14.3 32 32z" />
-              </svg>
+              <CircleExclamation className={styles.remittance__svg} />
             </div>
             <span className={styles.remittance__quotationText}>
               此訂單中所需支付的金額為 NT${renderInformation("total")}
@@ -181,7 +176,7 @@ export default function Remittance() {
               formatDate(
                 new Date(
                   history.information.timestamp.toDate().getTime() +
-                    3 * 24 * 60 * 60 * 1000
+                  3 * 24 * 60 * 60 * 1000
                 )
               )}{" "}
             23:59
