4083a8e5a9a2a1a216bff4a43d0cf786842c3bab
YuanShuoHsu <ook1725@gmail.com>
2024-07-29 21:57:26 +0800

    footer modified

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,7 +3,7 @@ import styles from "./index.module.scss";
 export default function Footer() {
   return (
     <div className={styles.footer}>
-      <p className={styles.footer__text}>Copyright © 2023 Yuan Shuo Hsu.</p>
+      <p className={styles.footer__text}>Copyright © 2024 Yuan Shuo Hsu.</p>
       <p className={styles.footer__text}>All rights reserved.</p>
     </div>
   );
