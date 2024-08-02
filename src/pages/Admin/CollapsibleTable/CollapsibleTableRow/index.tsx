43740f2b6260a9ad05482b404dfe7d6c5fa61fe4
YuanShuoHsu <ook1725@gmail.com>
2024-08-02 18:00:16 +0800

    temp add

--- src/pages/Admin/CollapsibleTable/CollapsibleTableRow/index.tsx
+++ src/pages/Admin/CollapsibleTable/CollapsibleTableRow/index.tsx
@@ -1,4 +1,4 @@
-import { useState, Fragment } from "react";
+import { useState, useRef, useEffect, Fragment } from "react";
 
 import { formatDate, formatTime } from "../../../../utils/formatDateTime";
 
@@ -54,76 +54,76 @@ const CollapsibleTableRow: React.FC<CollapsibleTableRowProps> = ({ userId, order
 
   return (
     <Fragment>
-      <tr className={styles.tbodyRow}>
+      <tr className={`${styles.tbodyRow} ${open ? styles.open : ""}`}>
         <td className={styles.tbodyCell}>
-          <AngleDown  className={styles.angleDown} onClick={() => setOpen(!open)} />
+          <div className={styles.iconWrapper}>
+            <AngleDown className={`${styles.angleDown} ${open ? styles.open : ""}`} onClick={() => setOpen(!open)} />
+          </div>
         </td>
         <td className={styles.tbodyCell}>{userId}</td>
       </tr>
-      {open && (
-        <tr>
-          <td colSpan={2} style={{ padding: 0 }}>
-            <div style={{ margin: '1em' }}>
-              <h6>Orders</h6>
-              <table style={{ width: '100%' }}>
-                <thead>
-                  <tr>
-                    <th>Date</th>
-                    <th>Sum</th>
-                    <th>Delivery Fee</th>
-                    <th>Address</th>
-                    <th>Remark</th>
-                    <th>Payment</th>
-                    <th>Send Method</th>
-                    <th>Customer Name</th>
-                    <th>Email</th>
-                    <th>Phone Number</th>
-                    <th>Discount</th>
-                    <th>Products</th>
+      <tr className={`${styles.collapsibleTbodyRow} ${open ? styles.open : ""}`}>
+        <td colSpan={2} className={styles.collapsibleTbodyCell}>
+          <div className={`${styles.collapsibleTbodyCellContent} ${open ? styles.open : ""}`}>
+            <h6>Orders</h6>
+            <table style={{ width: '100%' }}>
+              <thead>
+                <tr>
+                  <th>Date</th>
+                  <th>Sum</th>
+                  <th>Delivery Fee</th>
+                  <th>Address</th>
+                  <th>Remark</th>
+                  <th>Payment</th>
+                  <th>Send Method</th>
+                  <th>Customer Name</th>
+                  <th>Email</th>
+                  <th>Phone Number</th>
+                  <th>Discount</th>
+                  <th>Products</th>
+                </tr>
+              </thead>
+              <tbody>
+                {orders.map(({ information: { address, deliveryFee, discount, email, firstName, lastName, pay, phoneNumber, remark, send, sum, timestamp, }, product }, orderIndex) => (
+                  <tr key={orderIndex}>
+                    <td>
+                      {formatDate(new Date(timestamp.seconds * 1000))}{" "}
+                      {formatTime(new Date(timestamp.seconds * 1000))}
+                    </td>
+                    <td>{sum}</td>
+                    <td>{deliveryFee}</td>
+                    <td>{address}</td>
+                    <td>{remark}</td>
+                    <td>{pay}</td>
+                    <td>{send}</td>
+                    <td>{lastName} {firstName}</td>
+                    <td>{email}</td>
+                    <td>{phoneNumber}</td>
+                    <td>{discount ?? "N/A"}</td>
+                    <td>
+                      {product.map(({ images, name, number, price, selectedColor, selectedSize }, productIndex) => (
+                        <div key={productIndex} style={{ marginBottom: "10px" }}>
+                          <p>{name}</p>
+                          <p>Size: {selectedSize}</p>
+                          <p>Color: {selectedColor}</p>
+                          <p>Quantity: {number}</p>
+                          <p>Price: ${price}</p>
+                          <img
+                            src={images.main.src}
+                            alt={images.main.alt}
+                            width="50"
+                          />
+                        </div>
+                      ))}
+                    </td>
                   </tr>
-                </thead>
-                <tbody>
-                  {orders.map(({ information: { address, deliveryFee, discount, email, firstName, lastName, pay, phoneNumber, remark, send, sum, timestamp, }, product }, orderIndex) => (
-                    <tr key={orderIndex}>
-                      <td>
-                        {formatDate(new Date(timestamp.seconds * 1000))}{" "}
-                        {formatTime(new Date(timestamp.seconds * 1000))}
-                      </td>
-                      <td>{sum}</td>
-                      <td>{deliveryFee}</td>
-                      <td>{address}</td>
-                      <td>{remark}</td>
-                      <td>{pay}</td>
-                      <td>{send}</td>
-                      <td>{lastName} {firstName}</td>
-                      <td>{email}</td>
-                      <td>{phoneNumber}</td>
-                      <td>{discount ?? "N/A"}</td>
-                      <td>
-                        {product.map(({ images, name, number, price, selectedColor, selectedSize }, productIndex) => (
-                          <div key={productIndex} style={{ marginBottom: "10px" }}>
-                            <p>{name}</p>
-                            <p>Size: {selectedSize}</p>
-                            <p>Color: {selectedColor}</p>
-                            <p>Quantity: {number}</p>
-                            <p>Price: ${price}</p>
-                            <img
-                              src={images.main.src}
-                              alt={images.main.alt}
-                              width="50"
-                            />
-                          </div>
-                        ))}
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
-          </td>
-        </tr>
-      )}
-    </Fragment>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </td>
+      </tr>
+    </Fragment >
   );
 }
 
